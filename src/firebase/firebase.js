b1ea49499266d47cd7474306c5f362c4f37e8928
Payton Dennis <dennispayton15@gmail.com>
2021-01-28 17:52:59 -0600

    Integrate React App with Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'; 
+ 
+var firebaseConfig = {
+    apiKey: "AIzaSyDfxNAzdtpZyejOLHx5ynWfZRDnDvGKtCk",
+    authDomain: "scool-8772f.firebaseapp.com",
+    projectId: "scool-8772f",
+    storageBucket: "scool-8772f.appspot.com",
+    messagingSenderId: "600913176590",
+    appId: "1:600913176590:web:867d1ed5b38197fa64745f",
+    measurementId: "G-5MGLJPCDFK"
+};
+
+firebase.initializeApp(firebaseConfig); 
